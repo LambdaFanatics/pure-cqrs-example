d9c61f73c86a43d79a35301f568f13e60f92fc8a
Fotis Paschos <fpaschos@gmail.com>
2018-02-22 12:28:18 +0200

    Moving all the runnable programs (experiments) to examples project.

---
+++ examples/src/main/scala/RunProgramsInMemory.scala
@@ -0,0 +1,28 @@
+package apps
+
+import cats.effect.IO
+import infrastructure.repository.inmemory.PlantStoreInMemoryInterpreter
+
+
+// These should be converted to tests
+object RunProgramsInMemory extends App {
+
+  import programs._
+
+  var repo = PlantStoreInMemoryInterpreter[IO]()
+
+  val res1 = fetchPlants(repo).unsafeRunSync()
+  println(res1)
+
+  repo = PlantStoreInMemoryInterpreter[IO]()
+
+  val res2 = createAndFetchPlants(repo).unsafeRunSync()
+  println(res2)
+
+  repo = PlantStoreInMemoryInterpreter[IO]()
+
+  val res3 = createDeleteAndFetch(repo).unsafeRunSync()
+  println(res3)
+}
+
+
